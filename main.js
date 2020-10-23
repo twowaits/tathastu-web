6ad92ab0ecfc272fa3c320e7454d0833d320bcd8
Abhishek Sharma <abhi243461@hotmail.com>
2020-10-23 17:24:17 +0530

    feat: Add accordion

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ $('.nav-container').on('click', () => {
     else
         $('.nav-container').addClass('show-nav')
 })
+
+let acc = document.querySelectorAll(".accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
